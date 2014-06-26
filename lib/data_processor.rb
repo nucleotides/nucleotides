f96418a3972000a06fecf0c6e660cab2efd79f8b
Michael Barton <mail@michaelbarton.me.uk>
2014-06-25 18:23:45 -0700

    Reorganise data processing library
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- lib/data_processor.rb
+++ lib/data_processor.rb
@@ -1,8 +1,16 @@
 module DataProcessor
 
-  private
-
   DATA_DIR = 'tmp/'
+
+  FIELDS = {
+    '# contigs (>= 1000 bp)'   => :n_contigs_gt_1000,
+    'Genome fraction (%)'      => :genome_fraction,
+    'N50'                      => :n50,
+    'L50'                      => :l50,
+    '# mismatches per 100 kbp' => :mismatches_per_100k,
+    '# N\'s per 100 kbp'       => :ns_per_100k,
+    '# indels per 100 kbp'     => :indels_per_100k
+  }
 
   def files
     Dir[DATA_DIR + '*'].map do |file|
@@ -11,7 +19,15 @@ module DataProcessor
     end
   end
 
-  public
+  def parse_result(result)
+    require 'csv'
+    csv = CSV.parse(result, col_sep: "\t")
+    csv.inject({}) do |hash, (k, v)|
+      field = FIELDS[k]
+      hash[field] = v.to_f if field
+      hash
+    end
+  end
 
   def results
     files.group_by{|i| i[:data] }.map do |dataset, values|
@@ -24,29 +40,9 @@ module DataProcessor
     end
   end
 
-  def format_results
+  def execute!
     require 'yaml'
     puts YAML.dump results
-  end
-
-  FIELDS = {
-    '# contigs (>= 1000 bp)'   => :n_contigs_gt_1000,
-    'Genome fraction (%)'      => :genome_fraction,
-    'N50'                      => :n50,
-    'L50'                      => :l50,
-    '# mismatches per 100 kbp' => :mismatches_per_100k,
-    '# N\'s per 100 kbp'       => :ns_per_100k,
-    '# indels per 100 kbp'     => :indels_per_100k
-  }
-
-  def parse_result(result)
-    require 'csv'
-    csv = CSV.parse(result, col_sep: "\t")
-    csv.inject({}) do |hash, (k, v)|
-      field = FIELDS[k]
-      hash[field] = v.to_f if field
-      hash
-    end
   end
 
 end
