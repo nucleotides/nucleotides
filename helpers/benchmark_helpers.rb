090dd697843a605ce984bc057f805db40af450da
Michael Barton <mail@michaelbarton.me.uk>
2014-10-05 13:58:20 -0700

    Format numbers in benchmark tables
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- helpers/benchmark_helpers.rb
+++ helpers/benchmark_helpers.rb
@@ -1,5 +1,37 @@
 module BenchmarkHelpers
 
+  VALUES = {
+    proc:      [:metadata, :prc],
+    ng50:      [:summary_metrics, :ng50],
+    lg50:      [:summary_metrics, :lg50],
+    contigs:   [:summary_metrics, :n_contigs_gt_1000],
+    fraction:  [:summary_metrics, :genome_fraction],
+    incorrect: [:summary_metrics, :incorrect_per_100k],
+    cpu:       [:summary_metrics, :total_cpu_hours],
+    memory:    [:summary_metrics, :max_memory_gbytes],
+  }
+
+  FORMATTERS = {
+    ng50:      -> (v) {round_nearest(v, 1000)},
+    lg50:      -> (v) {round_nearest(v, 1)},
+    contigs:   -> (v) {round_nearest(v, 1)},
+    fraction:  -> (v) {sprintf "%.2f", v},
+    incorrect: -> (v) {sprintf "%.2f", v},
+    cpu:       -> (v) {sprintf "%.2f", v},
+    memory:    -> (v) {sprintf "%.2f", v},
+  }
+
+  def table_value(name, v)
+    value = VALUES[name].inject(v){|value, method| value.send method}
+    raise ArgumentError "No value #{name}" if value.nil?
+
+    if fmt = FORMATTERS[name]
+      fmt.call(value)
+    else
+      value
+    end
+  end
+
   def data_id(id)
     id.to_s.rjust(4, "0")
   end
@@ -8,7 +40,7 @@ module BenchmarkHelpers
     datum = data.genomes[data_id(id).to_sym]
     raise ArgumentError, "No genome metrics found for dataset #{id}" if datum.nil?
 
-    gc    = round_gc datum[:gc]
+    gc    = round_nearest(datum[:gc], 10)
     size  = round_size datum[:size]
 
     "Microbe - approx. size: #{size}MBp, approx. GC: #{gc}%"
@@ -20,8 +52,8 @@ module BenchmarkHelpers
 
 end
 
-def round_gc(v)
-  (v / 10).round * 10
+def round_nearest(v, n)
+  (v / n).round * n
 end
 
 def round_size(v)
