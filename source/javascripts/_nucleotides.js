1865126ced033108d0ebaffadb0452cc71766a11
Michael Barton <mail@michaelbarton.me.uk>
2014-10-08 09:38:01 -0700

    Hide maintainer column
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- source/javascripts/_nucleotides.js
+++ source/javascripts/_nucleotides.js
@@ -2,13 +2,13 @@ $(document).ready(function()
   {
 
     $('.score').dataTable({
-      order          : [[ 2, "desc" ]],
+      order          : [[ 3, "desc" ]],
       bFilter        : false,
       bInfo          : false,
       scrollY        : "400px",
       scrollCollapse : true,
       paging         : false,
-      columnDefs     : [ { visible        : false, targets : [3,7,8] } ],
+      columnDefs     : [ { visible : false, targets : [0,4,8,9] } ],
       dom            : 'C<"clear">lfrtip'
     });
 
