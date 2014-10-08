0922678066df53f71fea3eb1e3fb5761f0282dbc
Michael Barton <mail@michaelbarton.me.uk>
2014-10-08 09:13:13 -0700

    Add scrolling tables
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- source/javascripts/_nucleotides.js
+++ source/javascripts/_nucleotides.js
@@ -2,8 +2,12 @@ $(document).ready(function()
   {
 
     $('.score').dataTable({
-      "order": [[ 2, "desc" ]],
-      "bFilter": false
+      "order"          : [[ 2, "desc" ]],
+      "bFilter"        : false,
+      "bInfo"          : false,
+      "scrollY"        : "400px",
+      "scrollCollapse" : true,
+      "paging"         : false
     });
 
     $('.key').popover({
