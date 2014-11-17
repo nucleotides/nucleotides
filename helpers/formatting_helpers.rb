259fb4af0126a3e413d25f3ae6562964f4814028
Michael Barton <mail@michaelbarton.me.uk>
2014-11-16 16:03:32 -0800

    Columns for each image
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- helpers/formatting_helpers.rb
+++ helpers/formatting_helpers.rb
@@ -1,5 +1,18 @@
 module FormattingHelpers
 
+  def maintainer(string)
+    string.split('/')[0]
+  end
+
+  def image(string)
+    string.split('/')[1]
+  end
+
+  def bundle(string)
+    string.split('/')[2]
+  end
+
+
   def format_decimal(v)
     if v.nil?
       "No Value"
